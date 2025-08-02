ae8e3d6adccaca81a02c757423b2f9d71b9f2b8f
sombit-k <sombitkarmakar018@gmail.com>
2025-08-02 09:02:41 +0530

    update: regenerated Prisma client for ticket management schema

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -257,7 +257,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
