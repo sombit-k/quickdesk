5edaa12f15eb4fdf5118e410ec0a18a5748c2d28
sombit-k <sombitkarmakar018@gmail.com>
2025-08-02 15:27:02 +0530

    feat: Update NavbarLogo text from 'Startup' to 'Quickdesk'

--- components/ui/resizable-navbar.jsx
+++ components/ui/resizable-navbar.jsx
@@ -201,7 +201,7 @@ export const NavbarLogo = () => {
         alt="logo"
         width={30}
         height={30} />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+      <span className="font-medium text-black dark:text-white">Quickdesk</span>
     </Link>)
   );
 };
