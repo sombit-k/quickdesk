7a85d759a88d1aca933197607af68252e50a6b0f
sombit-k <sombitkarmakar018@gmail.com>
2025-08-02 16:21:32 +0530

    feat: Refactor HomePage component and enhance ticket filtering with new features

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { checkUser } from "@/lib/checkUser";
-export default async function Footer() {
-  await checkUser();
+
+export default function Footer() {
   return (
     <footer className="bg-black text-gray-300 py-8 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
